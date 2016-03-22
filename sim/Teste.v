59cb7732d909f443d785bb5630fb879053aba01f
JussaraGomes <jussarauefs@gmail.com>
2016-03-22 14:47:47 -0300

    Inicio das descrições dos módulos
    Estou descrevendo segundo o slide lá de exemplo

---
+++ sim/Teste.v
@@ -0,0 +1,20 @@
+/*Este módulo existe apenas para testar as classes, será descartado no fim*/
+include "Memoria.v";
+
+module Teste();
+integer count_stop = 0;
+reg clock;
+
+Memoria Memoria(.clk(clock));
+
+initial	clock = 1;
+	
+always #5 clock = ~clock;
+
+
+  always begin	
+	#1
+	if(count_stop>=1200) $stop;
+	count_stop = count_stop+1;
+  end
+endmodule
